93f7f13fd573ab7b4a15af6538ac3b243829ae1d
Vinicius Costa <vinicius.dsc95@gmail.com>
2022-11-14 23:05:19 -0300

    chore: added eslint MDX dependence

--- src/context/GlobalContex.tsx
+++ src/context/GlobalContex.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { lightTheme, darkTheme } from '../shared';
+import React from 'react';
+import { lightTheme } from '../shared';
 
 export const GlobalContext = React.createContext({});
 
@@ -7,14 +7,9 @@ export const GlobalStorage = ({ children }: { children: React.ReactNode }) => {
   const [theme, setTheme] = React.useState(lightTheme);
 
   function themeToggle() {
-    const currentTheme = theme.name === 'light' ? darkTheme : lightTheme;
-    setTheme(currentTheme);
+    // const currentTheme = theme.name === 'light' ? darkTheme : lightTheme;
+    // setTheme(currentTheme);
   }
-
-  useEffect(() => {
-    const { matches } = window.matchMedia('(prefers-color-scheme: light)');
-    matches ? setTheme(lightTheme) : setTheme(darkTheme);
-  }, []);
 
   return (
     <GlobalContext.Provider value={{ theme, setTheme, themeToggle }}>

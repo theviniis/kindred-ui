d12c265c5a081762e6d78c36ca8a15f6e95031f2
Vinicius Costa <vinicius.dsc95@gmail.com>
2022-11-15 02:05:53 -0300

    feat: Toast skeleton

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as T from './types';
+import * as S from './style';
+
+export const Toast: React.FC<T.ToastProps> = () => {
+  return <S.Wrapper>Toast</S.Wrapper>;
+};

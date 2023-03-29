8ca12bd92e32010f16968e4b7b0917ce2b1b2615
Vinicius Costa <vinicius.dsc95@gmail.com>
2023-03-28 21:06:42 -0300

    fix: typescript crypto error

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,3 +3,9 @@ import { palette } from '../shared';
 export type SKINS = keyof typeof palette;
 
 export type SIZES = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+declare global {
+  interface Crypto {
+    randomUUID: () => string;
+  }
+}
